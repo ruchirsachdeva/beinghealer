c96892c2b3c8b3db36f0036cdd7470ac29923a8a
Ruchir Sachdeva <ruchirsachdeva@yahoo.com>
2018-03-05 06:23:02 +0530

    Added navbar, footer, routes, landing page support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AppRoutingModule } from './app.routing';
+import {RouterModule} from "@angular/router";
+import {FooterComponent} from "./shared/footer/footer.component";
+import {NavbarComponent} from "./shared/navbar/navbar.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+/*
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';*/
+import { LandingComponent } from './landing/landing.component';
+
+const routes: Routes =[
+  /*  { path: 'home',             component: HomeComponent },
+    { path: 'user-profile',     component: ProfileComponent },
+    { path: 'signup',           component: SignupComponent },
+    { path: 'nucleoicons',      component: NucleoiconsComponent },*/
+    { path: 'landing',          component: LandingComponent },
+    { path: '', redirectTo: 'landing', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
