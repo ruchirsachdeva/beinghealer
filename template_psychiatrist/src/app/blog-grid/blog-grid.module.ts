be74bb72a7fee2f9737df1f92145183175fc6eb9
Ruchir Sachdeva <ruchirsachdeva@gmail.com>
2022-11-30 14:41:45 +0100

    Added search capability for blogs and truncated blog description in cards view

--- template_psychiatrist/src/app/blog-grid/blog-grid.module.ts
+++ template_psychiatrist/src/app/blog-grid/blog-grid.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogGridRoutingModule } from './blog-grid-routing.module';
 import { BlogGridComponent } from './blog-grid.component';
+import {Ng2SearchPipeModule} from "ng2-search-filter";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [BlogGridComponent],
-  imports: [CommonModule, BlogGridRoutingModule],
+  imports: [CommonModule, BlogGridRoutingModule, FormsModule, Ng2SearchPipeModule],
 })
 export class BlogGridModule {}
--- template_psychiatrist/src/app/blog/blog.module.ts
+++ template_psychiatrist/src/app/blog/blog.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import {Ng2SearchPipeModule} from "ng2-search-filter";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [BlogComponent],
-  imports: [CommonModule, BlogRoutingModule],
+  imports: [CommonModule, BlogRoutingModule, FormsModule, Ng2SearchPipeModule],
 })
 export class BlogModule {}
