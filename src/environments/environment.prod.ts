021a7044f460e7ace9325f3afb2db613cf16a143
Ruchir Sachdeva <ruchirs@spotify.com>
2023-02-13 18:56:02 +0100

    Updated ec2 instance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   localServer: "http://localhost:8080/api/",
   // bootServer: "http://beinghealerserver-env.eba-nzacycyz.ap-south-1.elasticbeanstalk.com"
-  bootServer: "http://ec2-13-126-242-155.ap-south-1.compute.amazonaws.com/"
+  bootServer: "http://ec2-43-205-119-73.ap-south-1.compute.amazonaws.com/"
 };
